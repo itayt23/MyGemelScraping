402eb4b54afc05cff70c810168abf1680fbfee5a
Amit Berger <bergeramit@gmail.com>
2021-09-24 00:33:27 +0300

    Split to functions

--- main.py
+++ main.py
@@ -3,33 +3,44 @@ import numpy as np
 
 OUTPUT_COLUMNS = ['שם', 'מסלול', 'אפיק', 'תשואה חודשית', 'תשואה 12 חודשים', 'תשואה מצטברת', 'דמי ניהול']
 
-def scrape(name, course, typee):
+def scrape(course, typee):
     return 1, 2, 3, 4
 
-def main():
-    df = pandas.read_excel('Clients.xlsx')
-    output_rows = []
-    output_df = pandas.DataFrame()
-
-    for i in range(df.shape[0]):
-        name, course, typee = df.iloc[i]
+def process_clients(clients):
+    results = {}
+    output = []
+    for i in range(clients.shape[0]):
+        name, course, typee = clients.iloc[i]
         print(f'Scraping for {name[::-1]} | {course[::-1]} | {typee[::-1]}')
-        monthly_return, ytd_return, accumulated_return, fees = scrape(name, course, typee)
+
+        if (course, typee) in results:
+            monthly_return, ytd_return, accumulated_return, fees = results[(course, typee)]
+        else:
+            monthly_return, ytd_return, accumulated_return, fees = scrape(course, typee)
+            results[(course, typee)] = monthly_return, ytd_return, accumulated_return, fees
+
         print(f'Results')
         print(f'monthly_return: {monthly_return}')
         print(f'ytd_return: {ytd_return}')
         print(f'accumulated_return: {accumulated_return}')
         print(f'fees: {fees}')
         print()
-        output_rows.append([name, course, typee, monthly_return,\
-            ytd_return, accumulated_return, fees])
 
-    # Saving the current columns with additional scapred info
-    output_df = pandas.DataFrame(np.array(output_rows), columns=OUTPUT_COLUMNS)
+        output.append([name, course, typee, monthly_return,\
+            ytd_return, accumulated_return, fees])
+    return output
+
+def write_output_to_excel(output, output_excel_name):
+    output_dataframe = pandas.DataFrame(np.array(output), columns=OUTPUT_COLUMNS)
     print(f'Final Table:')
-    print(output_df)
-    with pandas.ExcelWriter("Client_results.xlsx") as excel_writer:
-        output_df.to_excel(excel_writer)
+    print(output_dataframe)
+    with pandas.ExcelWriter(output_excel_name) as excel_writer:
+        output_dataframe.to_excel(excel_writer)
+
+def main():
+    clients_dataframe = pandas.read_excel('Clients.xlsx')
+    output = process_clients(clients_dataframe)
+    write_output_to_excel(output, "Client_results.xlsx")
 
 
 if __name__ == "__main__":
